6567428b857e26bcc2b6a1f9f52b6c28f37614e1
samueljmillward <samuel.j.pearce@hotmail.com>
2020-10-31 17:45:44 +0000

    Highscore functionality

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1
-
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -16,6 +16,12 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor= '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore= score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
     } else if (guess > secretNumber) {
         if (score > 1) {
         document.querySelector('.message').textContent = '📈 Too high!'
